ba9faf8b4d6bd7cb5ea2ec8414a017cc371a5501
emgord <emgord@gmail.com>
2016-02-01 16:37:33 -0800

    draw board works, need to eliminate zeros

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -20,35 +20,47 @@ Game.prototype.addTile = function() {
   this.board[row][column] = value;
 };
 
-// Game.prototype.moveTile = function(tile, direction) {
-//   // Game method here
-//   switch(direction) {
-//     case 38: //up
-//       console.log('up');
-//       break;
-//     case 40: //down
-//       console.log('down');
-//       break;
-//     case 37: //left
-//       console.log('left');
-//       break;
-//     case 39: //right
-//       console.log('right');
-//       break;
-//   }
-// };
-//
-// $(document).ready(function() {
-//   console.log("ready to go!");
-//   // Any interactive jQuery functionality
-//   var game = new Game();
-//
-//   $('body').keydown(function(event){
-//     var arrows = [37, 38, 39, 40];
-//     if (arrows.indexOf(event.which) > -1) {
-//       var tile = $('.tile');
-//
-//       game.moveTile(tile, event.which);
-//     }
-//   });
-// });
+Game.prototype.moveTile = function(tile, direction) {
+  // Game method here
+  switch(direction) {
+    case 38: //up
+      console.log('up');
+      break;
+    case 40: //down
+      console.log('down');
+      break;
+    case 37: //left
+      console.log('left');
+      break;
+    case 39: //right
+      console.log('right');
+      break;
+  }
+};
+
+Game.prototype.drawBoard = function(){
+  for(var row = 0; row < this.board.length; row++) {
+    for(var col = 0; col < this.board.length; col++)
+    $("#gameboard").append($("<div class=\"tile\" data-row=\"r" + row + "\", data-col=\"c" + col + "\" data-val=\"" + this.board[row][col] + "\">" + this.board[row][col] + "</div>"));
+  }
+};
+
+Game.prototype.clearBoard = function(){
+
+};
+
+$(document).ready(function() {
+  console.log("ready to go!");
+  // Any interactive jQuery functionality
+  var game = new Game();
+  game.drawBoard();
+
+  $('body').keydown(function(event){
+    var arrows = [37, 38, 39, 40];
+    if (arrows.indexOf(event.which) > -1) {
+      var tile = $('.tile');
+
+      game.moveTile(tile, event.which);
+    }
+  });
+});
